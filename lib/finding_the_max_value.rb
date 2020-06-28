1e4db3087d8518ebd8e8fa26c16caa7ade0b0170
Brad Beggs <brad.beggs@gmail.com>
2020-06-28 23:56:22 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -11,7 +11,7 @@ def find_max_value(array)
     else
       compare_var += 1
       puts "in the else statement" array[compare_var]
-      puts  #+ ",blah blah "+ array[index]
+      
     end
   end
 
