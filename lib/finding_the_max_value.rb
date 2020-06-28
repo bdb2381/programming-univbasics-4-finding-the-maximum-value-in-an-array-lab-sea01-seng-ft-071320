4a1ba415e24874753214081e22d1c6df12a0c87c
Brad Beggs <brad.beggs@gmail.com>
2020-06-28 23:44:31 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,31 +1,31 @@
 def find_max_value(array)
-  index = 0 
+  index = 0
   compare_var = 0
 
-  
-  array.size.times do 
-    if array[compare_var] >= array[index]
-      index += 1 
-    end  
 
-  else compare_var += 1 
-    
-  
+  array.size.times do
+    if array[compare_var] >= array[index]
+      index += 1
+    end
+
+  else compare_var += 1
+
+
 end
 
-  
-=begin 
+
+=begin
   loop through each index
   assigned index[0] to compare_Var
    if compare_var  >= array[index]
      increase index by 1 since compare_Var is bigger then current index data
      compare_var stays the same
    else compare_var needs increased by one index
-=end 
+=end
 
 
 
 
 
-  
+
 end
