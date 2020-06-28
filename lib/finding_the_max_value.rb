a2a6e36928cd9a6b52853852e3d256b8932365ac
Brad Beggs <brad.beggs@gmail.com>
2020-06-28 23:50:01 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -6,7 +6,7 @@ def find_max_value(array)
   array.size.times do
     if array[compare_var] < array[index]
       index += 1
-      puts array[compare_vary] + ", "+ array[index]
+      puts array[compare_vary] + ",blah blah "+ array[index]
 
     else
       compare_var += 1
